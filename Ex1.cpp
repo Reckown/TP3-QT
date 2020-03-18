725d5cbfdb55c14461d05da3b6f1dc83e2d9d51d
Tristan-Roux <tristan.roux@isen-ouest.yncrea.fr>
2020-03-18 14:23:25 +0100

    1

---
+++ Ex1.cpp
@@ -0,0 +1,5 @@
+//
+// Created by rouxt on 18/03/2020.
+//
+
+#include "Ex1.h"
---
+++ Ex1.h
@@ -0,0 +1,14 @@
+//
+// Created by rouxt on 18/03/2020.
+//
+
+#ifndef TP3_EX1_H
+#define TP3_EX1_H
+
+
+class Ex1 {
+
+};
+
+
+#endif //TP3_EX1_H
